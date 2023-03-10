cf246294d979acd9cd85cb2c27c896456ccd0f62
keeprich <35325509+keeprich@users.noreply.github.com>
2023-03-10 03:26:28 +0000

    Update dockerInstall.sh

--- dockerInstall.sh
+++ dockerInstall.sh
@@ -9,6 +9,6 @@ sudo yum remove docker docker-client docker-client-latest docker-common docker-l
 sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo -y
 sudo yum install docker-ce docker-ce-cli containerd.io -y
-sudo systemctl status docker
 sudo systemctl start docker
 sudo systemctl enable docker
+sudo systemctl status docker
